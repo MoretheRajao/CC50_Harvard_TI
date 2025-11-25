d85f2be49ef53683f92c989a0d8ce57e800aaf90
Morethe <138829272+MoretheRajao@users.noreply.github.com>
2025-11-25 18:13:23 -0300

    Update cash.c

--- Módulo_01_C/Exercicio_03/cash.c
+++ Módulo_01_C/Exercicio_03/cash.c
@@ -1,1 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
 
+int main(void)
+{
+    float troco;
+    int i, centavos;
+    int contador=0;
+    int moedas [] = {25,10,5,1};
+    do{
+
+        troco = get_float("troco:");
+    }
+    while(troco <=0);
+
+    centavos = round(troco * 100);
+
+    for(i = 0; i < 5; i++)
+    {
+        while(centavos - moedas [i] >=0 && centavos > 0){
+            centavos = centavos - moedas[i];
+            contador++;
+        }
+    }
+    printf("%i\n",contador);
+}
