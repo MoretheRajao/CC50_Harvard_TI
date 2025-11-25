3005966386dcb2c18f53d62bf9d03bd13ebd1bee
Morethe <138829272+MoretheRajao@users.noreply.github.com>
2025-11-25 18:12:19 -0300

    Update mario_desafio.c

--- Módulo_01_C/Exercicio_02/mario_desafio.c
+++ Módulo_01_C/Exercicio_02/mario_desafio.c
@@ -1,1 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int tijolos, i, j, espacos;
+    do
+    {
+        tijolos = get_int("Tamanho:");
+    }
+    while (tijolos < 1 || tijolos > 8);
+
+    for (i = 0; i < tijolos; i++)
+    {
+        for (espacos = 0; espacos < (tijolos - i - 1); espacos++)
+        {
+            printf(" ");
+        }
+        for (j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
